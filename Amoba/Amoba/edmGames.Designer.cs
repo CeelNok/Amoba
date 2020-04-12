aa7e46a6792905d5bc931e306894f1c406ed402a
Ádám Patrik <56996423+CeelNok@users.noreply.github.com>
2020-04-12 18:26:40 +0200

    Add database datas

--- Amoba/Amoba/edmGames.Designer.cs
+++ Amoba/Amoba/edmGames.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Patrik\Github\Amoba\amobagame\Amoba\Amoba\Amoba\edmGames.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Patrik\Documents\GitHub\Amoba\Amoba\Amoba\edmGames.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
